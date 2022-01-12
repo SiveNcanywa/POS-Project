a37112e5e86c73c5b4180c5db5a257dd32fbca43
SiveNcanywa <jeandreross@gmail.com>
2022-01-12 16:31:35 +0200

    changes

--- script.js
+++ script.js
@@ -30,57 +30,74 @@ function displayProduct(products){
     document.getElementById("milk").innerHTML = "";
 
     products.forEach((products, position) => {
+
         document.getElementById("milk").innerHTML += `
-        <div class="modal fade" id="exampleModal${position}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="col-6 col-sm-4" ><div class="card" style="width: 18rem;">
+            <img src="${products.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${products.title}</h5>
+              <p class="card-text">${products.price}</p>
+              <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#update-modal-${position}" >Update</button>
+              <button onclick="deleteProduct(${position})"  class="btn btn-danger">Delete</button>
+            </div>
+          </div>
+
+          <div class="modal fade" id="update-modal-${position}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <h5 class="modal-title" id="exampleModalLabel">update milkshake</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              Milkshakes<input type="text" id="update-input-${position}" value="${products.catergory}"/>
-              Catergory<input type="text" id="update-type-${position}" value="${products.price}"/>
-
+              <select name="type" id="update-cartegory-${position}" placeholder="cartegory">
+              <option value="Vanilla special.">Vanilla special.</option>
+                    <option value="Strawberry marshallow."> Strawberry marshallow.</option>
+                    <option value="Cool mint."> Cool mint.</option>
+                    <option value="Blueberry cheesecake.">Blueberry cheesecake.</option>
+                    <option value="Raspberry and white chocolate.">  Raspberry and white chocolate.</option>
+                    <option value="Triple nut caramel.">Triple nut caramel.</option>
+                    <option value="Pina colada.">strawberry milkshake</option>
+                    <option value="Pina colada.">Freak Rambling Litchi shake</option>
+                    <option value="Pina colada.">Oreo dark chocolate milkshake</option>
+                    <option value="Pina colada.">Roman creams&caramel milkshake</option>
+              </select>
+              <input type="text" id="update-price-${position}"placeholder="price"/>
+              <input type="text" id="update-title-${position}"placeholder="title"/>
+              <input type="text" id="update-img-${position}"placeholder="image"/>
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" data-bs-dismiss="modal" onclick="updateResturant(${position})"${products.catergory} >Save changes</button>
+              <button type="button" data-bs-toggle="modal" data-bs-target="#update-modal-${position}" onclick="updateProduct(${position})">Save changes</button>
             </div>
           </div>
         </div>
-      </div>
+      </div>  
+      
+      
 
-
-
-
-
-
-
-          <div class="col-6 col-sm-4"><div class="card" style="width: 18rem;">
-            <img src="${products.img}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title" >${products.title}</h5>
-              <p class="card-text">${products.price}</p>
-              <button   onclick="updateProduct()" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal${position}" >Update</button>
-              <button onclick="deleteProduct()"  class="btn btn-primary">Delete</button>
-            </div>
-          </div>
+      
         `;
     });
 }
 displayProduct(products);
 
 function createProduct(){
-  let myProduct = document.getElementById("milk").value;
+  // let myProduct = document.getElementById("milk").value;
   let catergory = document.getElementById("addCatergory").value;
+  let price=document.getElementById("price").value;
+  let img=document.getElementById("resturant").value;
+  let title=document.getElementById("title").value;
 
   
   try {
-      if(!myProduct)throw new Error("No milkshake input")
-      resturants.push({
-          name:myProduct,
-          type,});
+      // if(!myProduct)throw new Error("")
+      products.push({
+        catergory,
+        price,
+        img,
+        title,
+        });
       displayProduct(products);
   } catch (error) {
       
@@ -95,22 +112,21 @@ function deleteProduct(position){
 
 
 function updateProduct(position){
-  let product = document.querySelector(`#update-input-${position}`).value;
-  let type= document.querySelector(`#update-type-${position}`).value;
-      console.log(product, type)
-  try {
-      if(!product)throw new Error(`Please type in a milkshake at ${position} before updating`);
-      products[position] = {
-          name:product,
-          type,
-      };
-      displayProduct(products);
-  
-  
-  } catch (error) {
-      
-      alert(error)
+  // let product = document.querySelector(`#update-input-${position}`).value;
+  let price= document.querySelector(`#update-price-${position}`).value;
+  let cartegory=document.querySelector(`#update-cartegory-${position}`).value;
+  let img=document.querySelector(`#update-img-${position}`).value;
+  let title=document.querySelector(`#update-title-${position}`).value;
+
+
+  products[position] = {
+    cartegory,
+    price,
+    img,
+    title,
+
   }
+      displayProduct(products);
   
 }
 
